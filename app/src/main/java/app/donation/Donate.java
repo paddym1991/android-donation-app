81b4e64051768a1daab87a52abba48d71ab87ba6
Paddym1991 <paddym1991.pm@gmail.com>
2017-09-20 11:01:35 +0100

    added static menu and associated toast statements

--- app/src/main/java/app/donation/Donate.java
+++ app/src/main/java/app/donation/Donate.java
@@ -3,6 +3,9 @@ package app.donation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -73,6 +76,29 @@ public class Donate extends AppCompatActivity {
             String totalDonatedStr = "Total so far: €" + totalDonated;
             totalText.setText(totalDonatedStr);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_donate, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menuReport:
+                Toast.makeText(this, "Report Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuSettings:
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
 
