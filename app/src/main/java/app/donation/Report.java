b9ae29266d49ba24dfab0c10cdca473e581e4016
Paddym1991 <paddym1991.pm@gmail.com>
2017-09-22 12:05:25 +0100

    Added menu into Report page. Brings user back to Donate screen and displays Toast message for settings

--- app/src/main/java/app/donation/Report.java
+++ app/src/main/java/app/donation/Report.java
@@ -3,14 +3,19 @@ package app.donation;
 import app.donation.R;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,6 +35,24 @@ public class Report extends AppCompatActivity
         listView = (ListView) findViewById(R.id.reportList);
         DonationAdapter adapter = new DonationAdapter (this, app.donations);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_report, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId())
+        {
+            case R.id.menuDonate: startActivity(new Intent(this, Donate.class));
+                break;
+            case R.id.menuSettings:  Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
 
@@ -66,4 +89,5 @@ class DonationAdapter extends ArrayAdapter<Donation>
     {
         return donations.size();
     }
+
 }
